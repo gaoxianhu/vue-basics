1e73708fe3cfa011d04b10327af8f3b040ea64c3
gaoxianhu <3094571921@qq.com>
2020-05-27 21:56:00 +0800

    first

---
+++ src_testWebpack/index.js
@@ -0,0 +1,20 @@
+import indexCss from './public/css/index.css'
+import imgSrc from './public/imgs/3.jpg' //本质上拿到的是图片的路径
+
+let pp = document.createElement('p')
+pp.innerText = '渊哥喜欢赵丽颖'
+document.getElementById('root').appendChild(pp)
+
+let myFunc = () => {
+    console.log('哈哈')
+}
+myFunc()
+
+
+//创建图片
+let imgNode = new Image()
+imgNode.src = imgSrc
+document.getElementById('root').appendChild(imgNode)
+
+let a = 100
+console.log(a)
